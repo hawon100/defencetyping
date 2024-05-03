5a086c952bb0b47d9adb80de77a8458a6e463465
hawon100 <hawonoj002@gmail.com>
2024-05-03 21:01:32 +0900

    Map Move & Zoom

---
+++ Assets/Scripts/Core/Scenes/MapSelectScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelectScene : BaseScene
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        SceneType = Define.Scene.MapSelect;
+    }
+
+    public override void Clear()
+    {
+
+    }
+}
--- Assets/Scripts/Core/Utils/Define.cs
+++ Assets/Scripts/Core/Utils/Define.cs
@@ -55,6 +55,7 @@ public class Define
         InGame,
         TestHawon,
         TestKangmai,
+        MapSelect,
     }
 
     public enum Sound
