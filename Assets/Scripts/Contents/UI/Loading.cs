604e050bb916aa91734bcd8725263c9ce38a828d
hawon100 <hawonoj002@gmail.com>
2024-05-25 20:58:01 +0900

    TestScene

--- Assets/Scripts/Contents/UI/Loading.cs
+++ Assets/Scripts/Contents/UI/Loading.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Loading : MonoBehaviour
 {
     public Slider progressbar;
-    public Text loadtext;
+
+    static string GetSceneName(Define.Scene type)
+    {
+        string name = System.Enum.GetName(typeof(Define.Scene), type);
+        return name;
+    }
 
     private void Start()
     {
-        StartCoroutine(LoadScene());
+        StartCoroutine(LoadScene(Define.Scene.MapSelect));
     }
 
-    private IEnumerator LoadScene()
+    private IEnumerator LoadScene(Define.Scene type)
     {
         yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(GetSceneName(type));
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            yield return null;
+            if(progressbar.value < 1f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+            }
+
+            if(progressbar.value >= 1f && operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
     }
 }
