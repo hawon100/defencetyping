99cf1b18280edb8a39c8bc17722d32e9fe00143e
CloudAscend <bkm4763kk@gmail.com>
2024-07-10 08:47:33 +0900

    Temporary

--- Assets/Scripts/Contents/Tower/InstallTowerStat.cs
+++ Assets/Scripts/Contents/Tower/InstallTowerStat.cs
@@ -27,23 +27,23 @@ public class InstallTowerStat : TowerStat
         }
 
         if (Managers.Game.uiCanvas == null) Managers.Game.uiCanvas = GameObject.Find("TowerStatUI").transform; //Temp
-        GameObject u = Managers.Resource.Instantiate("UI/TowerHP/TowerHP", Managers.Game.uiCanvas);
-        hpUI = u.GetComponent<TowerStatHPUI>();
+        //GameObject u = Managers.Resource.Instantiate("UI/TowerHP/TowerHP", Managers.Game.uiCanvas);
+        //hpUI = u.GetComponent<TowerStatHPUI>();
         //hpUI.MoveUI(new Vector2(4, 4));
-        hpUI.SetUI(towerPos, Managers.Game.uiCanvas.GetComponent<Canvas>());
-        hpUI.InitHP(MaxHp, MaxHp);
+        //hpUI.SetUI(towerPos, Managers.Game.uiCanvas.GetComponent<Canvas>());
+        //hpUI.InitHP(MaxHp, MaxHp);
     }
 
     public override void OnAttacked(int damagedHp)
     {
-        hpUI.UpdateHP(Hp - damagedHp);
+        //hpUI.UpdateHP(Hp - damagedHp);
         base.OnAttacked(damagedHp);
     }
 
     protected override void OnDead()
     {
         Managers.Resource.Destroy(this.gameObject);
-        Managers.Resource.Destroy(hpUI.gameObject);
+        //Managers.Resource.Destroy(hpUI.gameObject);
         base.OnDead();
     }
 
