6a68789acd0452ebc2f181dccb4e9596473a5ded
hawon100 <hawonoj002@gmail.com>
2024-07-18 09:16:13 +0900

    Button Sound

--- Assets/DialogueEditor/Assets/Scripts/UI/UIConversationButton.cs
+++ Assets/DialogueEditor/Assets/Scripts/UI/UIConversationButton.cs
@@ -24,6 +24,7 @@ namespace DialogueEditor
         public eButtonType ButtonType { get { return m_buttonType; } }
 
         // UI Elements
+        [SerializeField] private AudioClip click;
         [SerializeField] private TMPro.TextMeshProUGUI TextMesh = null;
         [SerializeField] private Image OptionBackgroundImage = null;
         private RectTransform m_rect;
@@ -108,6 +109,7 @@ namespace DialogueEditor
         {
             if (!ConversationManager.Instance.AllowMouseInteraction) { return; }
 
+            Managers.Sound.Play(click);
             DoClickBehaviour();
         }
 
